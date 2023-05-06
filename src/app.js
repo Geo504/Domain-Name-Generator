dc0b8332d799ea0b12d90bcaf2bac6e10ff64bc1
Geovanny Valladares <129303891+Geo504@users.noreply.github.com>
2023-05-06 10:24:34 +0000

    First version

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+//write your code here
+const pronoun = ["the", "our"];
+const adj = ["great", "big"];
+const noun = ["jogger", "racoon"];
+const add = [".com", ".org"];
+
+const getDomainsNames = (array1, array2, array3, array4) => {
+  let arrayDomainsName = "";
+
+  for (let i = 0; i < array1.length; i++) {
+    for (let x = 0; x < array2.length; x++) {
+      for (let y = 0; y < array3.length; y++) {
+        for (let z = 0; z < array4.length; z++) {
+          arrayDomainsName += `<p>${array1[i] +
+            array2[x] +
+            array3[y] +
+            array4[z]}</p>`;
+        }
+      }
+    }
+  }
+  return arrayDomainsName;
 };
+
+const arrayWithDomainsName = getDomainsNames(pronoun, adj, noun, add);
+
+document.getElementById("btnDomain").addEventListener("click", function() {
+  document.getElementById("damainNameid").innerHTML = arrayWithDomainsName;
+});
